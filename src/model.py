731efcb72949c24b6c9b8727e30c6b820f02d2cc
Michael Persson <mwpersson@outlook.com>
2026-02-08 21:22:34 -0800

    add a few more models with flattened data, add confusion matrix

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class FashionClassifier(nn.Module):
+class FashionClassifierVision(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
@@ -17,4 +17,60 @@ class FashionClassifier(nn.Module):
         x = x.view(-1, 64 * 7 * 7)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+        return x
+    
+class FashionClassifier0(nn.Module):
+    def __init__(self, input_size=28*28, hidden_size=64, num_classes=10):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class FashionClassifier1(nn.Module):
+    def __init__(self, input_size=28*28, hidden_size=128, num_classes=10):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_classes)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+class FashionClassifier2(nn.Module):
+    def __init__(self, input_size=28*28, hidden_size=256, num_classes=10):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, num_classes)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+    
+class FashionClassifier3(nn.Module):
+    def __init__(self, input_size=28*28, hidden_size=512, num_classes=10):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.fc5 = nn.Linear(hidden_size, num_classes)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
         return x
